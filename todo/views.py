9ef6bf3a0fb26e600260dc276ab63403fcaa2a3d
Lyubomila <lubomilag1@gmail.com>
2021-02-21 14:32:25 +0000

    Creating new item

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,10 @@ from .models import Item
 # Create your views here.
 def get_todo_list(request):
     results = Item.objects.all()
-    return render(request, "todo/todo_list.html", {
+    context = {
         'items': results
-    })
+    }
+    return render(request, "todo/todo_list.html", context)
+
+def add_item(request):
+    return render(request, "todo/add_item.html")
